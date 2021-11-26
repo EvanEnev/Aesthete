74eb97d07204f3633c0de26bdab7eb414ba54b96
EvanEnev <evanenev@outlook.com>
2021-11-26 09:10:49 +0300

    Fix error when server does not have public updates channel

--- Interactions/Utils/update.js
+++ Interactions/Utils/update.js
@@ -15,7 +15,21 @@ module.exports = {
       .replaceAll('\\n', '\n');
 
     interaction.client.guilds.cache.forEach((guild) => {
-      const channel = guild.publicUpdatesChannel;
+      const channel =
+        guild.publicUpdatesChannel ||
+        guild.channels
+          .filter(
+            (channel) =>
+              channel
+                .permissionsFor(guild.client.user.id)
+                .has('SEND_MESSAGES') &&
+              channel
+                .permissionsFor(guild.client.user.id)
+                .has('VIEW_CHANNEL') &&
+              channel.type === 'GUILD_TEXT'
+          )
+          .sort((a, b) => a - b)
+          .first();
       channel.send({ content: message });
     });
   },
