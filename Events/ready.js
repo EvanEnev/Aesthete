425befbb5160a64afaed60f39a1b4e99511336a6
EvanEnev <evanenev@outlook.com>
2022-02-16 13:12:00 +0300

    Add if else statement for checking bot's ID

--- Events/ready.js
+++ Events/ready.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const devInteractions = require('../Utils/devInteractions');
 const commandsData = require('../Utils/interactions');
 const devInteraction = require('../Utils/devInteractions');
 
@@ -6,8 +7,12 @@ module.exports = {
   name: 'ready',
   async run(client) {
     console.log(chalk.blue("I'm Ready"));
-    await client.guilds.cache
-      .get('911568224221536337')
-      .commands.set(devInteraction);
+    if (client.user.id === '910633214245228614') {
+      await client.application.commands.set(commandsData);
+    } else {
+      await client.guilds.cache
+        .get('911568224221536337')
+        .commands.set(devInteractions);
+    }
   },
 };
