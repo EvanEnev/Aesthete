408271b80cdd1ed8887b740c769a53e59c12ccdf
wakayamadesu <100153087+wakayamadesu@users.noreply.github.com>
2022-02-21 20:00:19 +0200

    Украинская localization.js
    мейд бай вакаяма да

--- Utils/localization.js
+++ Utils/localization.js
@@ -2,23 +2,28 @@ module.exports = {
   description: {
     ru: 'Привет !\nМеня зовут **Эстет** и я создан для отыгрывания ролплей-действий прямо в Дискорде.\nЧтобы узнать мои команды просто пропиши `/` в текстовый канал.\n\nЧтобы изменить язык бота используй команду `/config language`\nTo change bots language use `/config language` command',
     en: 'Hi !\nMy name is **Aesthete** and I was created for role-playing right in the Discord.\nFor list of my commands type `/` in a text channel.\n\nTo change bots language use `/config language` command\nЧтобы изменить язык бота используй команду `/config language`',
+    uk: 'Привіт !\nМене звуть **Естет** і я створений для відігравання роплей-дій прямо в Дискорді.\nЩоб дізнатися мої команди просто пропиши `/` в текстовий канал.\n\nЩоб змінити мову бота використовуй команду `/config language ` 
   },
   inviteBot: {
     ru: 'Добавить к себе',
     en: 'Invite me',
+    uk: 'Додати до себе',
   },
-  voteForBot: { ru: 'Проголосуй за меня', en: 'Vote for me' },
+  voteForBot: { ru: 'Проголосуй за меня', en: 'Vote for me', 'Проголосуй за мене' },
   missingPermissions: {
     ru: '**У тебя нет прав для этого\nНедостающие права: **',
     en: "**You don't have permissions to do this\nMissing permissions: **",
+    uk: '**У тебе немає прав для цього\nВідсутні права: **',
   },
   rolePlayComplete: {
     ru: '**Действие успешно отправлено**',
     en: '**Action successfully submitted**',
+    uk: '**дія успішно відправлена **',
   },
   channelChanged: {
     ru: '**Теперь я буду отправлять сообщения с ролплей-действиями в**',
     en: '**Since now I will send role-play messages in**',
+    uk: '**Тепер я надсилатиму повідомлення з роплей-діями в**',
   },
   channelReseted: {
     ru: '**Канал для ролплей-действий сброшен**',
@@ -27,90 +32,111 @@ module.exports = {
   languageChanged: {
     ru: '**Теперь я буду этот язык: **',
     en: '**Since now I will use this language: **',
+    uk: '**Тепер я використовуватиму цю мову: **',
   },
   selectRoles: {
     selectRolesCreated: {
       ru: '**Выбор роли создан**',
       en: '**Select-roles created**',
+      uk: '**Вибір ролі зроблено**',
     },
     selectRolesRemoved: {
       ru: '**Выбор роли удалён**',
       en: '**Select-roles removed**',
+      uk: '**Вибір ролі видалено**',
     },
     selectRolesMessageEdited: {
       ru: '**Сообщение выбора роли изменёно**',
       en: '**Select-roles messge edited**',
+      uk: '**Повідомлення вибору ролі змінено**',
     },
     roleAdded: {
       ru: '**Роль добавлена**',
       en: '**Role added**',
+      uk: '**Роль додана**',
     },
     roleRemoved: {
       ru: '**Роль удалена**',
       en: '**Role removed**',
+      uk: '**Роль видалено**',
     },
-    roleChanged: { ru: '**Роль изменена**', en: '**Role edited**' },
+    roleChanged: { ru: '**Роль изменена**', en: '**Role edited**' uk: '**Роль змінена**' },
   },
   errors: {
     memberRequired: {
       ru: '**Действие требует указания участника**',
       en: '**This action required a member**',
+      uk: '**Дія вимагае вказівки учасника**',
     },
     rolePlayeWithBot: {
       ru: '**Нельзя совершить ролплей-действие с ботом**',
       en: '**You cannot use role-play action with a bot**',
+      uk: '**Не можна здійснити ролплей-дію з ботом**',
     },
     rolePlayeWithYourself: {
       ru: '**Нельзя совершить ролплей-действие с собой**',
       en: '**You cannot use role-play action with yourself**',
+      uk: '**Не можна здійснити ролплей-дію з собою**',
     },
     cannotSendMessages: {
       ru: '**Я не могу отправлять сообщения в этот канал**',
       en: '**I cannot send messages in this channel**',
+      uk: '**Я не можу надсилати повідомлення в цей канал**',
     },
     botAlredyUseThisLocale: {
       ru: '**Я уже использую этот язык**',
       en: '**I already use this language**',
+      uk: '**Я вже використивую цю мову**',
     },
     noMessageOrEmbed: {
       ru: '**Необходимо указать сообщение или эмбед**',
       en: '**You must specify a message or an embed**',
+      uk: '**Необхідно вказати повідомлення або ембед**',
     },
     unSupportedRole: {
       ru: '**Роль не должны быть ролью бота или @everyone**',
       en: '**The role must not be a bot or @everyone role**',
+      uk: '**Ви не повинні бути ролью бота або @everyone**',
     },
     noPermissionsForSelectroles: {
       ru: '**Я не могу добавить/убрать роль при выборе\nПроверьте, чтобы моя роль стояла выше**',
       en: '**I cannot assgin/remove role on select\nPut my role higher**',
+      uk: '**Я не можу додати/прибрати роль при виборі\nПеревірте, щоб моя роль стояла вище**',
     },
     noMessage: {
       ru: '**Я не могу найти это сообщение**',
       en: '**I cannot find this message**',
+      uk: '**Я не можу знайти це повідомлення**',
     },
     messageWithNoSelectRoles: {
       ru: '**У сообщения нет меню выбора роли**',
       en: '**This message does not contains select-roles menu**',
+      uk: '**У повідомлення немає меню вибору ролі**',
     },
     noLabel: {
       ru: '**Вы должны указать имя выбора**',
       en: '**You must specify the label**',
+      uk: '**Ви повинні вказати ім`я вибору**',
     },
     noNewRole: {
       ru: '**Вы должны указать новую роль**',
       en: '**You must specify the new role**',
+      uk: '**Ви повинні вказати нову роль**',
     },
     roleAlreadyAdded: {
       ru: '**Эта роль уже добавлена**',
       en: '**This role already added**',
+      uk: '**Ця роль вже додана**',
     },
     noRoleInMenu: {
       ru: '**Эта роль не добавлена**',
       en: '**This role has not been added**',
+      uk: '**Ця роль не додана**',
     },
     noOptions: {
       ru: '**Вы должны указать опцию для изменения**',
       en: '**You must specify the option to edit**',
+      uk: '**Ви повинні вказати опцію для зміни**',
     },
     tooManyRoles: {
       ru: '**В сообщение добавлено максимальное количество ролей (25)**',
