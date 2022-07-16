4e2295d790bb4cccd113514f2626c10fc815059c
EvanQube <evanenev@outlook.com>
2022-07-17 00:20:40 +0300

    Remove `chalk`

--- Events/ready.js
+++ Events/ready.js
@@ -1,17 +1,17 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const devInteractions = require('../Utils/devInteractions');
 const commandsData = require('../Utils/interactions');
 module.exports = {
-  name: 'ready',
-  async run(client) {
-    console.log(chalk.blue("I'm Ready"));
-    if (client.user.id === '910633214245228614') {
-      await client.application.commands.set(commandsData);
-    } else {
-      await client.application.commands.set([]);
-      ['911568224221536337', '721426713372000306'].forEach(async (guildID) => {
-        await client.guilds.cache.get(guildID).commands.set(devInteractions);
-      });
-    }
-  },
+    name: 'ready',
+    async run(client) {
+        console.log("I'm Ready");
+        if (client.user.id === '910633214245228614') {
+            await client.application.commands.set(commandsData);
+        } else {
+            await client.application.commands.set([]);
+            ['911568224221536337', '721426713372000306'].forEach(async (guildID) => {
+                await client.guilds.cache.get(guildID).commands.set(devInteractions);
+            });
+        }
+    },
 };
